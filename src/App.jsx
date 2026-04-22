2f52ff4255bcafdc35d2359b67ca795aad3d39e9
Tymoteusz Zydkiewicz <tymek.zydkiewicz.szkola@gmail.com>
2026-04-22 15:06:04 +0200

    starting first layout done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import PokemonCard from './components/PokemonCard';
+
 function App() {
+  const pokemons = [
+    { id: 1, name: 'Bulbasaur', type: 'grass' },
+    { id: 4, name: 'Charmander', type: 'fire' },
+    { id: 7, name: 'Squirtle', type: 'water' },
+    { id: 25, name: 'Pikachu', type: 'electric' },
+    { id: 39, name: 'Jigglypuff', type: 'fairy' },
+    { id: 52, name: 'Meowth', type: 'normal' },
+  ];
+
   return (
-    <div>
-      <h1>Pokedex</h1>
-      <p>My first React app</p>
+    <div className="app">
+      <div className="container">
+        <h1 className="page-title">Pokedex</h1>
+        <p className="page-description">
+          My first React Pokedex app
+        </p>
+
+        <div className="pokemon-grid">
+          {pokemons.map((pokemon) => (
+            <PokemonCard
+              key={pokemon.id}
+              id={pokemon.id}
+              name={pokemon.name}
+              type={pokemon.type}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
